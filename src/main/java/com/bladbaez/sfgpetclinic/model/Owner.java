8f95655fd9f0c65fe693dc8fd66b743aa9f373c0
Blad <bladimirbaezt@gmail.com>
2022-08-15 08:56:00 -0400

    Adding POJOs. Closes #2.

---
+++ src/main/java/com/bladbaez/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package com.bladbaez.sfgpetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/com/bladbaez/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.bladbaez.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
